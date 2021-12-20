c60305c74509b620c9fc36537f3401d6de10f048
Juan Treviranus <juantrevi70@gmail.com>
2021-12-20 18:14:47 -0300

    Initial commit

---
+++ src/main/java/Session/Login/AppUser/AppUserRole.java
@@ -0,0 +1,5 @@
+package Session.Login.AppUser;
+
+public enum AppUserRole {
+    USER, ADMIN;
+}
---
+++ src/main/java/Session/Login/Email/EmailSender.java
@@ -0,0 +1,5 @@
+package Session.Login.Email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
